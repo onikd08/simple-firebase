5ac9c22ceb5667dfece99e3583cc1e7d0bd51b31
Anik Das <onikd08@gmail.com>
2023-11-13 10:52:34 +0200

    Add login page and tailwind

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,5 +1,19 @@
+import { NavLink } from "react-router-dom";
+
 const Header = () => {
-  return <h1>Simple FireBase</h1>;
+  return (
+    <div>
+      <h1>Simple FireBase</h1>
+      <ul>
+        <li>
+          <NavLink to={"/"}>Home</NavLink>
+        </li>
+        <li>
+          <NavLink to={"/login"}>Login</NavLink>
+        </li>
+      </ul>
+    </div>
+  );
 };
 
 export default Header;
