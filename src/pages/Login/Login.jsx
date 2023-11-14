8a455901f033dafecd25a23f5cd3d9ff3dd08d94
Anik Das <onikd08@gmail.com>
2023-11-14 02:21:31 +0200

    Add login and logout

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,10 +1,39 @@
+import { getAuth, signInWithPopup, signOut } from "firebase/auth";
+import { app } from "../../firebase/firebase.init";
+import { GoogleAuthProvider } from "firebase/auth";
+import { useState } from "react";
+
 const Login = () => {
+  const [user, setUser] = useState(null);
+
+  const auth = getAuth(app);
+  const provider = new GoogleAuthProvider();
+
+  const handleGoogleLogin = () => {
+    signInWithPopup(auth, provider)
+      .then((result) => setUser(result.user))
+      .catch((error) => console.log(error.message));
+  };
+
+  const handleLogOut = () => {
+    signOut(auth)
+      .then((result) => {
+        console.log(result);
+        setUser(null);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
   return (
     <div>
       <h3 className="text-4xl my-10">This is Login</h3>
+      {user && user.displayName}
       <button
+        onClick={handleGoogleLogin}
         type="button"
-        className="py-2 px-4 flex justify-center items-center  bg-red-600 hover:bg-red-700 focus:ring-red-500 focus:ring-offset-red-200 text-white transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2  rounded-lg "
+        className="py-2 px-4 flex justify-center items-center  bg-blue-600 hover:bg-red-700 focus:ring-red-500 focus:ring-offset-red-200 text-white transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2  rounded-lg "
       >
         <svg
           width="20"
@@ -17,6 +46,13 @@ const Login = () => {
           <path d="M896 786h725q12 67 12 128 0 217-91 387.5t-259.5 266.5-386.5 96q-157 0-299-60.5t-245-163.5-163.5-245-60.5-299 60.5-299 163.5-245 245-163.5 299-60.5q300 0 515 201l-209 201q-123-119-306-119-129 0-238.5 65t-173.5 176.5-64 243.5 64 243.5 173.5 176.5 238.5 65q87 0 160-24t120-60 82-82 51.5-87 22.5-78h-436v-264z"></path>
         </svg>
         Google
+      </button>
+      <button
+        onClick={handleLogOut}
+        type="button"
+        className="py-2 px-4 bg-red-600 hover:bg-red-700 focus:ring-red-500 focus:ring-offset-red-200 text-white transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2  rounded-lg "
+      >
+        Logout
       </button>
     </div>
   );
